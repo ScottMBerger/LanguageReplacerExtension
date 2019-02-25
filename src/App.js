d8174017d866dc54cb7ed74de1d10d1ef04e9eef
ScottMBerger <contact@scottb.app>
2019-02-24 21:56:34 -0500

    Use storage sync to get stored values

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Button from '@material-ui/core/Button';
 import CheckboxListSecondary from './ListView/list.js'
+import TabBar from './components/TabBar.js'
 class App extends Component {
   render() {
     return (
-    <React.Fragment>
-      <CssBaseline />
-      <div className="App">
-      <CheckboxListSecondary></CheckboxListSecondary>
-        <div className="options">
-          <ul>
-            <li>
-              Test 
-              <Button variant="contained" color="primary">
-                Hello World
-              </Button>
-            </li>
-            <li>Test2</li>
-          </ul>
+      <React.Fragment>
+        <CssBaseline />
+        <div className="App">
+          <TabBar></TabBar>
         </div>
-      </div>
-    </React.Fragment>
+      </React.Fragment>
     );
   }
 }
